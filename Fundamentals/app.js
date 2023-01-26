cbfdc59fa5d56599ded6264eb0a114842c39ea93
Eizeek <azikarimouv@gmail.com>
2023-01-26 12:25:17 +0400

    ObjectTAsk

---
+++ Fundamentals/app.js
@@ -0,0 +1,93 @@
+
+// Task 1
+/*
+let mainObj = {
+    name: "Aziz",
+    surname: "Karimov"
+}
+
+let addObj = (key, value) => {
+
+    return mainObj[key] = value
+}
+
+
+let delObj =  (key) => {
+
+    return delete  mainObj[key]
+}
+
+addObj("age", 30)
+delObj("surname")
+
+console.log(mainObj)
+*/
+
+
+// Task 2  - ?
+
+// Task 3 
+/*
+let array = [
+          {
+            name: "Aziz",
+            count: 7
+          },
+          {
+            name: "Imran",
+            count: 2
+          },
+          {
+            name: "Zamir",
+            count: 5
+          },
+          {
+            name: "Eshqin",
+            count: 8
+          },
+          {
+            name: "Farid",
+            count: 1
+          }
+];
+array.sort((a, b) => a.count - b.count);
+
+console.log(array)
+*/
+
+// Task 4
+
+/*
+let obj = {
+    name: "Aziz",
+    age: 30
+}
+let newObj = Object.assign({}, obj)
+console.log(newObj)
+
+*/
+
+
+// Task 5
+/*
+let obj = {
+    name: "Aziz",
+    age: 30
+}
+let newObj = {...obj};
+
+newObj = obj;
+console.log(newObj)
+*/
+
+// Task 6
+/*
+function jpn (manga, chapter, year) {
+      this.anime = manga;
+      this.chap = chapter;
+      this.dec = year;
+    }
+    let newObj = new jpn("DragonBall", "Balls", 1998);
+    console.log(newObj);
+    */
+//Task 7 - ?
